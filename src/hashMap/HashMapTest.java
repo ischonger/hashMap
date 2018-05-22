6e9072ac4343654c90036a28ca7c605197d828b8
ischonger <i.schaefer@xceptance.net>
2018-05-22 17:12:13 +0200

    3 more tests; generalized map size

--- src/hashMap/HashMapTest.java
+++ src/hashMap/HashMapTest.java
@@ -10,10 +10,12 @@ public class HashMapTest
 {
     HashMap<String, Integer> map = new HashMap<String, Integer>();
 
+    int mapSize = 100;
+
     @Before
     public void setUp() throws Exception
     {
-        for (int i = 0; i < 100; i++)
+        for (int i = 0; i < mapSize; i++)
         {
             map.put(String.valueOf(i), i);
         }
@@ -22,10 +24,27 @@ public class HashMapTest
     @Test
     public void putHashMap()
     {
-        assertEquals(100, map.size());
+        assertEquals(mapSize, map.size());
 
         int actual = map.get("51");
         assertEquals(51, actual);
+    }
+
+    @Test
+    public void put2HashMap()
+    {
+        map.put("the_answer", 42);
+        int actual = map.get("the_answer");
+        assertEquals(actual, 42);
+    }
+
+    @Test
+    public void putAnotherMap()
+    {
+        HashMap<Integer, Boolean> boolMap = new HashMap<Integer, Boolean>();
+        boolMap.put(3, true);
+        Boolean actual = boolMap.get(3);
+        assertEquals(actual, true);
     }
 
     @Test
@@ -58,9 +77,9 @@ public class HashMapTest
         map.addAll(map2);
 
         int actual = map.size();
-        assertEquals(110, actual);
+        assertEquals(mapSize + 10, actual);
 
-        for (int i = map.size(); i < 110; i++)
+        for (int i = map.size(); i < mapSize + 10; i++)
         {
             assertEquals(map.get(String.valueOf(100 + i)), map2.get(String.valueOf(i)));
         }
@@ -73,4 +92,10 @@ public class HashMapTest
         assertEquals(expectedSize, 100);
     }
 
+    @Test
+    public void sizeOfEmptyHashMap()
+    {
+        HashMap<String, Integer> map = new HashMap<String, Integer>();
+        assertEquals(0, map.size());
+    }
 }
